05f239d962493220dfab28df1dcdadcfdc022f1f
Gaurav Pratap Singh <gs492979@gmail.com>
2025-08-23 02:08:23 +0530

    Login controller created.

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -5,18 +5,31 @@ const register = async (req, res) => {
     const { email, password } = req.body;
     try {
         const isUserAlreadyExisted = await User.findOne({ email });
-        if (isUserAlreadyExisted) return res.status(409).json({ message: "User already registered with this email" });
+        if (isUserAlreadyExisted) return res.status(409).json({ message: "A user is already registered with this email address." });
         const hashedPassword = await bcrypt.hash(password, 10);
         const createdUser = await User.create({
             email, password: hashedPassword
         })
         const token = jwt.sign({ email: createdUser.email, id: createdUser._id }, process.env.JWT_SECRET_KEY);
         res.cookie("token", token);
-        res.status(201).json({ message: "User registered successfully", user: createdUser });
+        res.status(201).json({ message: "User registered successfully.", user: createdUser });
     } catch (error) {
-        res.status(500).json({ message: "Something went wrong while registering new user", error: error.message });
+        res.status(500).json({ message: "An error occurred while registering the new user.", error: error.message });
     }
 }
 
-
-module.exports = { register };
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const foundUser = await User.findOne({ email });
+        if (!foundUser) return res.status(401).json({ message: "Invalid email or password." });
+        const isValidPassword = await bcrypt.compare(password, foundUser.password);
+        if (!isValidPassword) return res.status(401).json({ message: "Invalid email or password." })
+        const token = jwt.sign({ email: foundUser.email, id: foundUser._id }, process.env.JWT_SECRET_KEY);
+        res.cookie("token", token);
+        res.status(200).json({ message: "User logged in successfully.", user: foundUser })
+    } catch (error) {
+        res.status(500).json({ message: "An error occurred while logging in the user.", error: error.message });
+    }
+}
+module.exports = { register, login };
