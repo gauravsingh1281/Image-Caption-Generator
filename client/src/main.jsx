c1550539a97a48c578b8de1c124eae3ccb0915c1
Gaurav Pratap Singh <gs492979@gmail.com>
2025-09-29 19:36:52 +0530

    netlify redirect issue fixed.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,9 +6,7 @@ import { Provider } from "react-redux"
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
